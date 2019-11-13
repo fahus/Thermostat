a3244d04a191480f8c662f34e697cc437aae5f58
fahus <faduma.h@yahoo.com>
2019-11-13 19:45:30 +0000

    user story 3: decrease the temp with a down function

--- src/thermostat.js
+++ src/thermostat.js
@@ -5,8 +5,12 @@ function Thermostat() {
 }
 Thermostat.prototype.getCurrentTemperature = function () {
   return this.temperature;
-
 };
+
 Thermostat.prototype.up = function () {
   this.temperature += 1;
+};
+
+Thermostat.prototype.down = function () {
+  this.temperature -= 1;
 };
